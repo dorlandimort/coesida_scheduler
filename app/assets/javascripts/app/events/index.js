fa52131a3e0bbc0f42192f19ac3022973cfd9e15
Orlando Giovanni Martínez Dionisio <dorlandimort@gmail.com>
2020-01-16 17:14:01 -0600

    Update index.js

--- app/assets/javascripts/app/events/index.js
+++ app/assets/javascripts/app/events/index.js
@@ -14,8 +14,8 @@ function initCalendar() {
         slotEventOverlap: false,
         eventOverlap: false,
         slotDuration: '00:15:00', // 15 minutes
-        minTime: '07:00:00',
-        maxTime: '21:00:00',
+        minTime: '05:00:00',
+        maxTime: '23:59:00',
         defaultView: 'timeGridWeek',
         droppable: false,
         eventStartEditable: false,
